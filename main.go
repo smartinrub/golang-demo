42156b96dffe2d4c66ed7f14004bb7742a3f8d56
Sergio Martin <econsergio@gmail.com>
2018-02-08 00:51:33 +0000

    second handler added

--- main.go
+++ main.go
@@ -14,10 +14,18 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+func weatherHandler(w http.ResponseWriter, r *http.Request) {
+	// Read content of html file and returns a Template
+	t, _ := template.ParseFiles("weather.html")
+	// Execute the template, writing the generated HTML to the http.ResponseWriter
+	t.Execute(w, nil)
+}
+
 // Main
 func main() {
 	// Routers
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/weather", weatherHandler)
 
 	// setting listening port
 	err := http.ListenAndServe(":8080", nil)
